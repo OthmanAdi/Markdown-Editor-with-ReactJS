cf508378953e205ec777d6e684656f9a9b15626f
OthmanAdi <a.othman@mediaagentur-in.berlin>
2021-03-26 15:57:41 +0100

    25.03 Markdown editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [input, setInput] = useState();
+    return (
+        <div className="App">
+            <div className="cont">
+                <div className="nav">
+                    <VisibilityIcon />
+                    MARKDOWN
+                </div>
+                <textarea
+                    className="textarea"
+                    value={input}
+                    autoFocus
+                    onChange={(e) => setInput(e.target.value)}
+                ></textarea>
+            </div>
 
-export default App;
+            <div className="cont">
+                <div className="nav">
+                    <VisibilityIcon />
+                    PREIVEW
+                </div>
+                <ReactMarkdown
+                    source={input}
+                    renderers={{
+                        code: Component,
+                    }}
+                    className="markdown"
+                />
+            </div>
+        </div>
+    );
+}
+const Component = ({ value, language }) => {
+    const codeString = "(num) => num + 1";
+    return (
+        <SyntaxHighlighter language={language ?? null} style={docco}>
+            {value ?? ""}
+        </SyntaxHighlighter>
+    );
+};
+export default App
